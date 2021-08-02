9554a22c6ecaa5b1721091b06d79ddeab39daa83
truongquanghung <truongquanghunghn@gmail.com>
2021-08-02 10:55:48 +0700

    Passing Array from C++ to Python

--- test.cpp
+++ test.cpp
@@ -5,6 +5,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n=0;
+vector <int> a;
+
 extern "C" void print_array(double* array, int N)
 {
     for (int i=0; i<N; i++) 
@@ -14,4 +17,23 @@ extern "C" void print_array(double* array, int N)
 extern "C" int add_one(int i)
 {
     return i+1;
+}
+
+extern "C" void add(int i)
+{
+    n+=i;
+    a.push_back(n);
+}
+
+extern "C" int num()
+{
+    return a.size();
+}
+
+extern "C" int* out()
+{
+    int* res = new int[a.size()];
+    for (int i=0;i<a.size();i++)
+        res[i] = a[i];
+    return res;
 }
