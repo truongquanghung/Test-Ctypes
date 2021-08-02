43b070aac023d6d0ecb5b3597ce366b0ef67432d
truongquanghung <truongquanghunghn@gmail.com>
2021-08-02 16:53:48 +0700

    Pass string to store in vector in C++ and get data with Python

--- test.cpp
+++ test.cpp
@@ -1,27 +1,24 @@
-// extern "C" int add_one(int i)
-// {
-//     return i+1;
-// }
 #include <bits/stdc++.h>
 using namespace std;
 
-int n=0;
-vector <int> a;
+int n = 0;
+vector<int> a;
+vector<string> s;
 
-extern "C" void print_array(double* array, int N)
+extern "C" void print_array(double *array, int N)
 {
-    for (int i=0; i<N; i++) 
+    for (int i = 0; i < N; i++)
         cout << i << " " << array[i] << endl;
 }
 
 extern "C" int add_one(int i)
 {
-    return i+1;
+    return i + 1;
 }
 
 extern "C" void add(int i)
 {
-    n+=i;
+    n += i;
     a.push_back(n);
 }
 
@@ -30,10 +27,42 @@ extern "C" int num()
     return a.size();
 }
 
-extern "C" int* out()
+extern "C" int *out()
 {
-    int* res = new int[a.size()];
-    for (int i=0;i<a.size();i++)
+    int *res = new int[a.size()];
+    for (int i = 0; i < a.size(); i++)
         res[i] = a[i];
+    return res;
+}
+
+extern "C" void str(char *c, int n)
+{
+    // cout << n << endl;
+    // for (int i = 0; i < n; i++)
+    //     cout << c[i];
+    // cout << endl;
+    string st(c);
+    s.push_back(st);
+}
+
+extern "C" int num_str()
+{
+    return s.size();
+}
+
+extern "C" char ** out_str()
+{
+    char **res = new char*[s.size()];
+    for (int i = 0; i < s.size(); i++){
+        res[i] = new char[s[i].length()];
+        for (int j=0;j<s[i].length();j++) res[i][j] = s[i][j];
+    }
+    return res;
+}
+
+extern "C" int* get_size(){
+    int *res = new int[s.size()];
+    for (int i = 0; i < s.size(); i++)
+        res[i] = s[i].length();
     return res;
 }
--- test_ctypes.py
+++ test_ctypes.py
@@ -13,21 +13,33 @@ py_print_array.argtypes = [ctl.ndpointer(np.float64, flags='aligned, c_contiguou
 A = np.array([1.4, 2.6, 3.0], dtype=np.float64)
 py_print_array(A, 3)
 
+# while True:
+#     i = input()
+#     c_lib.add(int(i))
+#     size = c_lib.num()
+#     print("size: ",size)
+#     c_lib.out.restype = ctl.ndpointer(dtype=ctypes.c_int, shape=(size,))
+#     for x in c_lib.out():
+#         print(x)
+
+from ctypes import *
+
 while True:
-    i = input()
-    c_lib.add(int(i))
-    size = c_lib.num()
+    s = input()
+    print("--------------------------------------------")
+    print("Vector")
+    c_s = c_char_p(s.encode())
+    #  add string to vector
+    c_lib.str.argtypes = [c_char_p, c_int]
+    c_lib.str(c_s, len(s))
+
+    # get data from vector
+    size = c_lib.num_str() # size of vector
     print("size: ",size)
-    c_lib.out.restype = ctl.ndpointer(dtype=ctypes.c_int, shape=(size,))
-    for x in c_lib.out():
-        print(x)
-
-# libname = 'testlib.so'
-# libdir = './'
-# lib=ctl.load_library(libname, libdir)
-
-# py_add_one = lib.add_one
-# py_add_one.argtypes = [ctypes.c_int]
-# value = 5
-# results = py_add_one(value)
-# print(results)
+    c_lib.get_size.restype = ctl.ndpointer(dtype=ctypes.c_int, shape=(size,)) # size of each string
+    c_lib.out_str.restype = POINTER(c_char_p) # vector
+    res = c_lib.out_str()
+    array_size = c_lib.get_size()
+    for i in range(0,size):
+        print(res[i].decode("ascii", errors='ignore')[0:array_size[i]])
+    print("-------------------------------------------")
