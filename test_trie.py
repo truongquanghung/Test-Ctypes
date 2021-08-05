cbcbe9ebbdbfb261e8efdf9bc6de345b4e6f072f
truongquanghung <truongquanghunghn@gmail.com>
2021-08-05 10:24:53 +0700

    Compare C++ with Python when using Trie

--- test_trie.py
+++ test_trie.py
@@ -39,19 +39,29 @@ column = input()
 
 import timeit
 
-start = timeit.default_timer()
-
 sql = f"SELECT {column} from {table}"
 cursor = conn.execute(sql)
-
+s = []
 for row in cursor:
-    c_s = c_char_p(str(row[0]).encode('"ascii"', errors='ignore'))
+    s.append(str(row[0]))
+
+start = timeit.default_timer()
+for st in s:
+    c_s = c_char_p(st.encode('ascii', errors='ignore'))
     #  add string to trie
     c_lib.insert_trie.argtypes = [c_char_p]
     c_lib.insert_trie(c_s)
 
 stop = timeit.default_timer()
-print('Time: ', stop - start)  
+print('Time in C++: ', stop - start)  
+
+from trie import Trie
+start = timeit.default_timer()
+test = Trie()
+for st in s:
+    test.insert_key(st)
+stop = timeit.default_timer()
+print('Time in Python: ', stop - start)  
 
 while True:
     s = input()
@@ -61,7 +71,7 @@ while True:
     # get data from trie
     print("--------------------")
     print("Data:")
-
+    start = timeit.default_timer()
     c_lib.get_num_trie.argtypes = [c_char_p]
     size = c_lib.get_num_trie(c_s)
 
@@ -73,5 +83,11 @@ while True:
     res = c_lib.pass_data_trie(c_s)
     for i in range(0,size):
         print(res[i].decode('ascii', errors='ignore')[0:res_size[i]])
+    stop = timeit.default_timer()
+    print('Time response in C++: ', stop - start)  
     
+    start = timeit.default_timer()
+    print(test.get_data(s))
+    stop = timeit.default_timer()
+    print('Time response in Python: ', stop - start)  
     print("--------------------")
